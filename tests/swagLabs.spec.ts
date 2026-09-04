192d3d27aec0a06fdd4258a372011d96445b0e50
Paula <pauzam003@gmail.com>
2026-09-04 16:38:08 +0200

    Agrego test de login con credenciales invalidas

--- tests/swagLabs.spec.ts
+++ tests/swagLabs.spec.ts
@@ -3,10 +3,11 @@ import { expect, test } from "@playwright/test";
 test('Navegar a Swag Labs y validar el titulo ', async ({ page }) => {
     await test.step('Navegamos a Swag Labs', async () => {
         await page.goto('');
-        await expect (page).toHaveTitle('Swag Labs');
+
+        await expect(page).toHaveTitle('Swag Labs');
     });
-    
-    
+
+
 })
 test('Iniciar sesión con credenciales válidas', async ({ page }) => {
     await test.step('Login válido', async () => {
@@ -14,7 +15,20 @@ test('Iniciar sesión con credenciales válidas', async ({ page }) => {
         await page.getByTestId('username').fill('standard_user');
         await page.getByTestId('password').fill('secret_sauce');
         await page.getByTestId('login-button').click();
+
         await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
     });
-    
+
 });
+
+test('Login con credenciales inválidas', async ({ page }) => {
+    await test.step('Login inválido', async () => {
+        await page.goto('');
+        await page.getByTestId('username').fill('paula_user');
+        await page.getByTestId('password').fill('secret_sauces');
+        await page.getByTestId('login-button').click();
+
+        await expect(page.getByTestId('error')).toHaveText('Epic sadface: Username and password do not match any user in this service');
+
+    });
+})
