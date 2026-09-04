c1bd50713919b8faff8e27c003d5d74c038d948c
Paula <pauzam003@gmail.com>
2026-09-04 16:13:21 +0200

    agrego test de login exitoso

--- tests/swagLabs.spec.ts
+++ tests/swagLabs.spec.ts
@@ -2,9 +2,19 @@ import { expect, test } from "@playwright/test";
 
 test('Navegar a Swag Labs y validar el titulo ', async ({ page }) => {
     await test.step('Navegamos a Swag Labs', async () => {
-        await page.goto('https://www.saucedemo.com/');
+        await page.goto('');
         await expect (page).toHaveTitle('Swag Labs');
     });
     
     
 })
+test('Iniciar sesión con credenciales válidas', async ({ page }) => {
+    await test.step('Login válido', async () => {
+        await page.goto('');
+        await page.getByTestId('username').fill('standard_user');
+        await page.getByTestId('password').fill('secret_sauce');
+        await page.getByTestId('login-button').click();
+        await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+    });
+    
+});
