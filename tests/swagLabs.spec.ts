25a5d07bda19b875de40c06b77b781c8f03d413f
Paula <pauzam003@gmail.com>
2026-09-07 07:25:27 +0200

    Agrego test de checkout completo

--- tests/swagLabs.spec.ts
+++ tests/swagLabs.spec.ts
@@ -40,5 +40,37 @@ test('Agregar productos al carrito', async ({ loggedInPage }) => {
 
         await expect(loggedInPage.getByTestId('shopping-cart-badge')).toHaveText('2');
     
+})
+
+test('Realizar compra completa', async ({ loggedInPage }) => {
+    await test.step('Agregar 3 productos al carrito ', async () => {
+        await loggedInPage.getByTestId('add-to-cart-sauce-labs-backpack').click();
+        await loggedInPage.getByTestId('add-to-cart-sauce-labs-fleece-jacket').click();
+        await loggedInPage.getByTestId('add-to-cart-sauce-labs-onesie').click();
+
+        await expect(loggedInPage.getByTestId('shopping-cart-badge')).toHaveText('3');
+
+    });
+
+    await test.step('Damos click en el carrito y hacemos checkout', async () => {
+        await loggedInPage.getByTestId('shopping-cart-link').click();
+        await loggedInPage.getByTestId('checkout').click();
+    });
+    
+    await test.step('Llenamos el formulario de envío', async () => {
+        await loggedInPage.getByTestId('firstName').fill('Paula');
+        await loggedInPage.getByTestId('lastName').fill('Zambrano');
+        await loggedInPage.getByTestId('postalCode').fill('08205');
+        
+    });
+    await test.step('Finalizar compra', async () => {
+        await loggedInPage.getByTestId('continue').click();
+        await loggedInPage.getByTestId('finish').click();
+
+        await expect (loggedInPage.getByTestId('complete-header')).toHaveText('Thank you for your order!');
+    })
+    
+    
     
 })
+
